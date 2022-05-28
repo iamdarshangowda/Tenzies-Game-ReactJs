a8ba36669082e1883c8611aeeae760fd530dec7c
iamdarshangowda <iamdarshangowda@gmail.com>
2022-05-28 23:23:57 +0530

    added dies and random number

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
+import react, { useState } from "react";
 import "./App.css";
+import Die from "./Die";
 
 function App() {
-  return <div className="App"></div>;
+  const [dice, setDice] = useState(allNewDice());
+
+  function allNewDice() {
+    let newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push({
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: i,
+      });
+    }
+    return newDice;
+  }
+
+  function rollDice() {
+    setDice(allNewDice());
+  }
+
+  const diceElements = dice.map((die) => (
+    <Die key={die.id} value={die.value} held={die.isHeld} />
+  ));
+
+  return (
+    <main>
+      <div className="dice-container">{diceElements}</div>
+      <button className="roll-dice" onClick={rollDice}>
+        Roll
+      </button>
+    </main>
+  );
 }
 
 export default App;
---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function Footer() {
+  return (
+    <footer className="text-center m-5 ">
+      <a href="https://github.com/iamdarshangowda">
+        created by <span className="font-weight-bold">darshan gowda</span>
+      </a>
+    </footer>
+  );
+}
+
+export default Footer;
