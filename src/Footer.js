63b6bd3923a77aab466d19d013861d8f535e4a95
iamdarshangowda <iamdarshangowda@gmail.com>
2022-05-31 23:12:45 +0530

    main margin updated

--- src/Footer.js
+++ src/Footer.js
@@ -3,9 +3,10 @@ import "./App.css";
 
 function Footer() {
   return (
-    <footer className="text-center m-5 ">
-      <a href="https://github.com/iamdarshangowda">
-        created by <span className="font-weight-bold">darshan gowda</span>
+    <footer className="text-center m-5">
+      <a href="https://github.com/iamdarshangowda" className="text-info">
+        created by{" "}
+        <span className="font-weight-bold text-white">darshan gowda</span>
       </a>
     </footer>
   );
